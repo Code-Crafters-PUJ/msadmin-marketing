bb26cbc11fb6ee1e171b19174a083df387dc4611
Santicm23 <Santicastrom@outlook.com>
2024-03-31 10:57:01 -0500

    rewriting in typescript

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import cors from 'cors'
+import express, { Request, Response } from 'express'
+
+import mockData from './mockData'
+
+const app = express()
+const PORT = 3000
+
+app.use(cors())
+
+app.get('/api/active-users', (req: Request, res: Response): void => {
+  res.json({ percentage: mockData.emails.visualizacion })
+})
+
+app.get('/api/users-landing', (req: Request, res: Response): void => {
+  res.json({ users: mockData.landingPage.users })
+})
+
+app.get('/api/formas-de-enterarse', (req: Request, res: Response): void => {
+  res.json({ formasDeEnterarse: mockData.formasDeEnterarse })
+})
+
+app.get('/api/history-users', (req: Request, res: Response): void => {
+  res.json({ history: mockData.historialDudas })
+})
+
+app.listen(PORT, (): void => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
